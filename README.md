15782014dc6ff99183b46ce0927f28f0e25e0cb2
Delta456 <swstkbaranwal@gmail.com>
2019-09-19 16:08:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# V by Example
+
+> Learn V by Example
+
+Note: This is still a work in progress
+
+## Contributing
+
+For contributing see [CONTRIBUTING.md](CONTRIBUTING.md)
+
+## License
+
+[MIT](LICENSE)
---
+++ src/hello/hello.v
@@ -0,0 +1,19 @@
+// Single line comments
+
+/* This is a multiline comment.
+ * /* It can also be nested. */
+ *
+*/
+
+fn main() {
+
+    print('Hello V\n')
+
+    // This will print on a new line automatically
+    println('Hello V by Example')
+
+    /*println('I am Bob and I am $age old') */  // This won't print because `age` isn't defined.
+
+    panic('Exiting from the program')
+    
+}
