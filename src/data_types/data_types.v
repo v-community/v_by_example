a1fb58f60ce6eb6cb175599880fd10c293ae81f5
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2019-10-01 12:01:35 +0800

    add `data_types`  example

---
+++ src/data_types/data_types.v
@@ -0,0 +1,15 @@
+
+fn main() {
+
+	// Strings, which can be added together with `+`.
+	println("V" + "lang")
+
+	// Integers
+  print('1 + 1 = ')
+	println(1 + 1)
+
+	// Booleans, with boolean operators as you'd expect.
+	println(true && false)
+	println(true || false)
+	println(!true)
+}
