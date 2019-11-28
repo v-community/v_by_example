3a99c7cfdbf1cfe7bea51e2ea9caa95ea74fe542
Sven Patrick Meier <sven.meier@uni-ulm.de>
2019-11-29 00:53:34 +0100

    spaces

--- code/variables/variables.v
+++ code/variables/variables.v
@@ -1,8 +1,8 @@
 fn main() {
-	age := 23				// int
-	name := 'Alice'			// string
-	is_male := false		// bool
-	is_adult := age > 21	// bool
+    age := 23				// int
+    name := 'Alice'			// string
+    is_male := false		// bool
+    is_adult := age > 21	// bool
 
 	println(age)			// 23
 	println(name)			// Alice
